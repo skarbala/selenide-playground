1fc78f8a83e8d6d48a5af7a5a1f52e7530615572
skarbala <skarbala.martin@gmail.com>
2019-09-02 09:05:17 +0200

    Fix potterapi (#1)
    * add fix when potterapi is not available
    
    * displaying spells with lowercase

--- spelleology/index.js
+++ spelleology/index.js
@@ -1,8 +1,8 @@
 Vue.component('modal', {
     template: '#modal-template'
 });
-const baseUrl = 'https://www.potterapi.com/v1';
-const key = '$2a$10$f.wBgzvPPpAvJi0D1d1MOOC/uTEqHWqG6tGxfa/i2u.7ob7O9JGla';
+// const baseUrl = 'https://www.potterapi.com/v1';
+// const key = '$2a$10$f.wBgzvPPpAvJi0D1d1MOOC/uTEqHWqG6tGxfa/i2u.7ob7O9JGla';
 const vue = new Vue({
     el: '#app',
     data: {
@@ -12,14 +12,18 @@ const vue = new Vue({
         showModal: false
 
     },
+
     created: function () {
-        let url = new URL(baseUrl + '/spells'),
-            params = {key: key};
-        Object.keys(params).forEach(key => url.searchParams.append(key, params[key]));
-        fetch(url)
-            .then(response => response.json())
-            .then(data => this.spells = data);
+      setTimeout(function () {
+          this.getData()
+      }.bind(this), getRandomResponseTime());
+        // let url = new URL(baseUrl + '/spells'),
+        //     params = {key: key};
+        // Object.keys(params).forEach(key => url.searchParams.append(key, params[key]));
+
     },
+
+
     computed: {
         filteredList: function () {
             return this.spells.filter((spell) => {
@@ -31,6 +35,16 @@ const vue = new Vue({
         selectSpell: function (spell) {
             this.selectedSpell = spell;
             this.showModal = true;
-        }
-    }
+        },
+        getData: function(){
+          fetch("spelleology/spells.json")
+              .then(response => response.json())
+              .then(data => this.spells = data);
+        },
+    },
+
 });
+
+function getRandomResponseTime() {
+    return Math.round(Math.random() * (1500 - 1000) + 1000);
+}
